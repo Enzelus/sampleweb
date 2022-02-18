7de73dc517a6968e34a5169deb7d4064d41f86d3
Michael Dayo <Michaeldayo2703@gmail.com>
2022-02-18 22:29:00 +0800

    Start

---
+++ javascript.js
@@ -0,0 +1,43 @@
+
+
+// const prevScrollpos = window.pageYOffset;
+// let navbar = document.getElementsByClassName('navbar');
+
+// window.onscroll = function () {
+
+//     let currentScrollpos = window.pageYOffset;
+
+//     if (prevScrollpos > currentScrollpos) {
+
+//         navbar.style.top = '0';
+//     }
+
+//     else {
+//         navbar.style.top = '-50px';
+//     }
+
+//     prevScrollpos = currentScrollpos
+// }
+
+const navbar = document.getElementById('navbar');
+
+let prevScrolled = window.scrollY;
+
+addEventListener('scroll', () => {
+    
+
+    if (window.scrollY > prevScrolled) {
+        navbar.style.top = '-100px'
+        // console.log("We're Going Down!")
+    }
+
+    else {
+
+        // console.log("We're Going Up!")
+        navbar.style.top = "0"
+    }
+
+    prevScrolled = window.scrollY;
+
+})
+
